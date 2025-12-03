a27fde4b527e23bc9293b3402daba578f0a0198b
isHardCoded <ishardcoded@gmail.com>
2025-12-03 14:36:36 +0300

    fix text

--- src/components/Categories/renderCategories.js
+++ src/components/Categories/renderCategories.js
@@ -2,7 +2,7 @@ import CategoryItem from './CategoryItem.js'
 
 export function renderCategories(categories) {
 	if (!categories || !categories.length) {
-		return '<div class="empty-state">Категории не найдены</div>'
+		return '<div class="empty-state">Categories not found</div>'
 	}
 
 	return categories.map(category => CategoryItem(category)).join('')
--- src/components/Courses/renderCourses.js
+++ src/components/Courses/renderCourses.js
@@ -2,7 +2,7 @@ import CourseItem from './CourseItem.js'
 
 export function renderCourses(courses) {
 	if (!courses || !courses.length) {
-		return '<div class="empty-state"><p class="lato-regular">Курсы не найдены</p></div>'
+		return '<div class="empty-state"><p class="lato-regular">Courses not found</p></div>'
 	}
 
 	return courses.map(course => CourseItem(course)).join('')
